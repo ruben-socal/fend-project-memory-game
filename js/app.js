911eac3407c46c9be2f2b232456d502a8ce03ecf
Ruben <ruben2045@gmail.com>
2017-10-12 17:02:27 -0700

    refactor: added displayCardSymbol funtion that displays the card symbol, cardsMatch function checks if the cards match then displays the match, noCardMatch function temporary displays the cards that did not match and returns cards to drak gray, updateStarsDisplay function updates the stars display as the game is being played, updateMovesDisplay function updates the moves display as the game is being played.

--- js/app.js
+++ js/app.js
@@ -6,10 +6,15 @@ var cardList = ["fa fa-diamond","fa fa-paper-plane-o","fa fa-anchor","fa fa-bolt
 				"fa fa-bomb","fa fa-leaf","fa fa-bomb","fa fa-bolt",
 				"fa fa-bicycle","fa fa-paper-plane-o","fa fa-cube"];
 
+/* cardMatch keeps track of 2 cards being selected */
 var cardsToMatch = [];
 
-/* function check for card match */
-// function checkForMatch(currentCard) { }
+/* moveCounter keeps track the number of moves before winning the game */
+var moveCount = 0;
+
+/* matchCounter keeps track of the cards matched */
+var matchCount = 0;
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -32,13 +37,66 @@ function shuffle(array) {
     return array;
 }
 
-// add cards
+/* addCards function add each cards html to the page after shuffling cards */
 function addCards(array) {
 	let cards = shuffle(array);
 	$( ".card" ).each(function( index ) {
 		$(this).children().attr("class", cards[index]);
 	});
 }
+
+/* displayCardSymbol function displays card symbol */
+function displayCardSymbol (cardIcon) {
+	cardIcon.addClass("show open");
+	// cardIcon.off( "click");
+}
+
+/* updateMovesDisplay function updates the moves count display */
+function updateMovesDisplay(moveCount) {
+	$(".moves").text(moveCount);
+}
+
+/* updateStarDisplay function updates the stars display */
+function updateStarsDisplay(moveCount) {
+	if( moveCount === 13) {
+		$(".stars li:last-child").children().attr("class", "fa fa-star-o");
+	} else if(moveCount === 18) {
+		$(".stars li:nth-child(2)" ).children().attr("class", "fa fa-star-o");
+	} else if(moveCount === 20 ) {
+		$(".stars li:first-child()" ).children().attr("class", "fa fa-star-o")
+	}
+}
+
+/* resetStars function resets stars display to 3 */
+function resetStars() {
+	$(".stars li i").attr("class", "fa fa-star");
+}
+
+function cardsMatch(cardsToMatch,moveCount) {
+	let cardOne = cardsToMatch[0],
+	    cardTwo = cardsToMatch[1];
+	cardOne.addClass("match");
+	cardOne.removeClass("show open");
+	cardOne.off( "click");
+	cardTwo.addClass("match");
+	cardTwo.removeClass("show open");
+	cardTwo.off( "click"); // $this.on("click"); to turn event back on
+}
+
+function noCardMatch(cardsToMatch,moveCount) {
+	let cardOne = cardsToMatch[0],
+	    cardTwo = cardsToMatch[1];
+	cardOne.removeClass("show open");
+	cardTwo.addClass("nomatch");
+	cardOne.addClass("nomatch");
+ 	// cardOne.on( "click");
+	// use setTimeout to delay background change
+	setTimeout(function(){
+		cardTwo.removeClass("nomatch");
+	 	cardOne.removeClass("nomatch");
+	}, 1000);
+}
+
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -57,35 +115,32 @@ function addCardEvents() {
 	    event.preventDefault();
 	    let cardIcon = $(this);
 	    cardsToMatch.push(cardIcon);
-	    if( cardsToMatch.length === 1 ) {
-	    	$(this).addClass("show open");
+	    if( cardsToMatch.length === 1) {
+	    	displayCardSymbol(cardIcon);
+	    } else if ( matchCount === 16 ) {
+	    	/* TODO: 1. reset moveCount and matchCount 2. reset display for stars to 3 and move to 0
+	    			 3. display win screen with play again button 4. turn on click events on cards
+	    	*/
+	    	gameWin();
 	    } else {
 	    	/* check for match */
 	    	let cardOne = cardsToMatch[0],
 	    		cardTwo = cardsToMatch[1],
 	    		value1 = cardOne.children().attr("class"),
-	    		value2 = cardTwo.children().attr("class"),
-	    		$this = $(this); // remove if not used
-	    	/* if matched 1) change both card background colors to turquoise 2) disable click event */
+	    		value2 = cardTwo.children().attr("class");
+	    	/* if cards match 1) change both card background colors to turquoise 2) disable click event */
 	    	if( value1 === value2 ) {
-	    		cardOne.addClass("match");
-	    		cardOne.removeClass("show open");
-	    		cardOne.off( "click");
-	    		cardTwo.addClass("match");
-	    		cardTwo.removeClass("show open");
-	    		cardTwo.off( "click"); // $this.on("click"); to turn event back on
+	    		matchCount += 2;
+			    moveCount ++;
+	    		cardsMatch(cardsToMatch,moveCount);
 	    	} else {
-		    	/* if not matched 1) change both card background colors to red/orange for 1 second than back to solid dark gray 2) click events should still work*/
-		    	cardOne.removeClass("show open");
-		    	cardTwo.addClass("nomatch");
-		    	cardOne.addClass("nomatch");
-		    	// use setTimeout to delay background change
-		    	setTimeout(function(){
-		    		cardTwo.removeClass("nomatch");
-		    	 	cardOne.removeClass("nomatch");
-		    	}, 1000);
-
+		    	/* if cards do not match 1) change both card background colors to red/orange for 1 second than
+		    		back to solid dark gray 2) click events should still work */
+		    	moveCount ++;
+		    	noCardMatch(cardsToMatch,moveCount);
 	    	}
+	    	updateStarsDisplay(moveCount);
+	    	updateMovesDisplay(moveCount);
 	    	cardsToMatch = [];
 	    }
 	});
